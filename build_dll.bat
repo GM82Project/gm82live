8942adaacb02d996536f62f8015464beff3bc217
omicronrex <65692652+omicronrex@users.noreply.github.com>
2023-07-23 03:16:11 -0300

    use upx

--- build_dll.bat
+++ build_dll.bat
@@ -3,4 +3,6 @@ cargo build --target i686-pc-windows-msvc --release
 del gm82live.dll
 move .\target\i686-pc-windows-msvc\release\gm82live.dll gm82live.dll
 
+upx --lzma gm82live.dll
+
 pause
--- build_extension.bat
+++ build_extension.bat
@@ -1,4 +1,4 @@
-curl https://raw.githubusercontent.com/GM82Project/gm82gex/main/gm82gex.py -o gm82gex.py
+if not exist gm82gex.py curl https://raw.githubusercontent.com/GM82Project/gm82gex/main/gm82gex.py -o gm82gex.py
 if exist gm82live.dll gm82gex.py gm82live.gej
 
 pause
